9ae7f07a5822ee622092890d7beb1ddb4d07f679
Marlon <marlonne.pilario@gmail.com>
2023-09-04 18:42:11 +0800

    second commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import PropertyDetails from "./pages/PropertyDetails";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="propter/:id" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
